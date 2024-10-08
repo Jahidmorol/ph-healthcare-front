be4c567963849d0767b673e3ee2bb9f5dc4254f6
jahid <jahidmorol2@gmail.com>
2024-10-08 11:33:05 +0600

    implant hero section

--- src/components/Ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/Ui/HomePage/HeroSection/HeroSection.tsx
@@ -53,7 +53,78 @@ const HeroSection = () => {
           <Button variant="outlined">Contact us</Button>
         </Box>
       </Box>
-      <Box>left</Box>
+      <Box
+        sx={{
+          p: 1,
+          position: "relative",
+          flex: 1,
+          mt: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            left: "200px",
+            top: "-30px",
+          }}
+        >
+          <Image src={assets.svgs.arrow} width={100} height={100} alt="arrow" />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Box mt={4}>
+            <Image
+              src={assets.images.doctor1}
+              width={240}
+              height={380}
+              alt="doctor1"
+            />
+          </Box>
+          <Box>
+            <Image
+              src={assets.images.doctor2}
+              width={240}
+              height={350}
+              alt="doctor2"
+            />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "220px",
+            left: "150px",
+          }}
+        >
+          <Image
+            src={assets.images.doctor3}
+            width={240}
+            height={240}
+            alt="doctor3"
+          />
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-50px",
+            right: 0,
+            zIndex: "-1",
+          }}
+        >
+          <Image
+            src={assets.images.stethoscope}
+            width={180}
+            height={180}
+            alt="doctor3"
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
