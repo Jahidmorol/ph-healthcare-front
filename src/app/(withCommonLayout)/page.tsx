ca52ae77ceb14bad7154ca9bf5c599aea976fc3d
jahid <jahidmorol2@gmail.com>
2024-10-08 11:20:14 +0600

    implant hero section right side

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/Ui/HomePage/HeroSection/HeroSection";
 import { Button } from "@mui/material";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>This is Home</h2>
-      <Button>Contained</Button>
+      <HeroSection />
     </div>
   );
 };
--- src/components/Ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/Ui/HomePage/HeroSection/HeroSection.tsx
@@ -1,10 +1,59 @@
-import { Container } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
+import Image from "next/image";
 import React from "react";
+import assets from "@/assets";
 
 const HeroSection = () => {
   return (
-    <Container>
-      <h2>Hero section</h2>
+    <Container
+      sx={{
+        display: "flex",
+        direction: "row",
+        my: 16,
+      }}
+    >
+      <Box
+        sx={{
+          position: "relative",
+          flex: 1,
+        }}
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            width: "700px",
+            left: "-90px",
+            top: "-120px",
+          }}
+        >
+          <Image src={assets.svgs.grid} alt="grid" />
+        </Box>
+        <Typography variant="h2" component="h1" fontWeight={600}>
+          Healthier Hearts
+        </Typography>
+        <Typography variant="h2" component="h1" fontWeight={600}>
+          Come From
+        </Typography>
+        <Typography
+          variant="h2"
+          component="h1"
+          fontWeight={600}
+          color="primary.main"
+        >
+          Preventive Care
+        </Typography>
+        <Typography my={4}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit eum
+          iusto consequatur eius, doloribus nesciunt facere aliquid eveniet et.
+          Rerum maiores saepe cupiditate repellat recusandae atque sed. Saepe,
+          vitae id?
+        </Typography>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Button>Make appointment</Button>
+          <Button variant="outlined">Contact us</Button>
+        </Box>
+      </Box>
+      <Box>left</Box>
     </Container>
   );
 };
